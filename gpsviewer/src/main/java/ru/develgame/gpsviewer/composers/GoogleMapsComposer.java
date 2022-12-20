bf0ecc7292fe97298960d0e57a0e3b17cc28a77a
izemskov <pascal.ilya@gmail.com>
2022-12-20 14:57:10 +0300

    View all received dates

--- gpsviewer/src/main/java/ru/develgame/gpsviewer/composers/GoogleMapsComposer.java
+++ gpsviewer/src/main/java/ru/develgame/gpsviewer/composers/GoogleMapsComposer.java
@@ -4,28 +4,50 @@ import org.springframework.web.client.RestTemplate;
 import org.zkoss.gmaps.Gmaps;
 import org.zkoss.gmaps.Gmarker;
 import org.zkoss.gmaps.LatLng;
-import org.zkoss.gmaps.MapModel;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.VariableResolver;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.ListModelList;
 import ru.develgame.gpsdomain.GPSReceivedData;
-
-import java.util.List;
 
 @VariableResolver(org.zkoss.zkplus.spring.DelegatingVariableResolver.class)
 public class GoogleMapsComposer extends SelectorComposer<Component>{
 	@Wire
 	private Gmaps gmaps;
 
+	@Wire
+	private Combobox dateComboBox;
+
 	@WireVariable
 	private RestTemplate restTemplate;
+
+	private ListModelList<String> dateModel;
 
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
 
+		String[] allDates = restTemplate.getForObject("http://localhost:8111/getAllDates", String[].class);
+		if (allDates != null) {
+			for (String elem : allDates) {
+				getDateModel().add(elem);
+			}
+		}
+	}
+
+	public ListModelList<String> getDateModel() {
+		if (dateModel == null)
+			dateModel = new ListModelList<>();
+
+		return dateModel;
+	}
+
+	@Listen("onSelect = #dateComboBox")
+	public void dateComboBoxOnSelect() {
 		GPSReceivedData[] gpsReceivedData = restTemplate.getForObject("http://localhost:8111/data", GPSReceivedData[].class);
 		if (gpsReceivedData != null && gpsReceivedData.length > 0) {
 			gmaps.setCenter(new LatLng(gpsReceivedData[0].getLatitude(), gpsReceivedData[0].getLongitude()));
