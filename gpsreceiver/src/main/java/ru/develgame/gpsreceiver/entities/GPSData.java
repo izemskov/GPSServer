ad89d6dc7325d196cacb2c3509a31ec1d3cfa530
izemskov <pascal.ilya@gmail.com>
2022-12-21 15:26:04 +0300

    Server configuration

--- gpsreceiver/src/main/java/ru/develgame/gpsreceiver/entities/GPSData.java
+++ gpsreceiver/src/main/java/ru/develgame/gpsreceiver/entities/GPSData.java
@@ -1,12 +1,10 @@
 package ru.develgame.gpsreceiver.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
+@Table(name = "gpsdata")
 public class GPSData {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
