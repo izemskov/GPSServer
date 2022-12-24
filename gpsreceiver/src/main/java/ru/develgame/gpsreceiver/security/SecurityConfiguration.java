4815e30285a2b7e7de928a88ddf9e8e9127f1dcf
izemskov <pascal.ilya@gmail.com>
2022-12-25 00:34:51 +0300

    Basic authentication

---
+++ gpsreceiver/src/main/java/ru/develgame/gpsreceiver/security/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package ru.develgame.gpsreceiver.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Autowired
+    private UserDetailsService gpsServerUserDetailsService;
+
+    @Autowired
+    private DaoAuthenticationProvider gpsServerDaoAuthenticationProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(gpsServerUserDetailsService);
+        auth.authenticationProvider(gpsServerDaoAuthenticationProvider);
+    }
+}
